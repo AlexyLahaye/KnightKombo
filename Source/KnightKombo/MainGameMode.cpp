1b11c556be805e87e90d49d93586914ad2543be8
AlexyLahaye <90752843+AlexyLahaye@users.noreply.github.com>
2024-11-18 13:00:57 +0100

    Sprite Knight 2d

---
+++ Source/KnightKombo/MainGameMode.cpp
@@ -0,0 +1,43 @@
+﻿#include "MainGameMode.h"
+#include "CineCameraActor.h"
+#include "Kismet/GameplayStatics.h"
+#include "GameFramework/PlayerController.h"
+
+void AMainGameMode::BeginPlay()
+{
+	Super::BeginPlay();
+
+	SetupPlayerCamera();
+}
+
+void AMainGameMode::SetupPlayerCamera()
+{
+	// Récupérer le PlayerController
+	APlayerController* PlayerController = UGameplayStatics::GetPlayerController(GetWorld(), 0);
+	if (!PlayerController)
+	{
+		UE_LOG(LogTemp, Error, TEXT("PlayerController not found!"));
+		return;
+	}
+
+	// Trouver la caméra avec le tag "MainCamera"
+	TArray<AActor*> FoundActors;
+	UGameplayStatics::GetAllActorsWithTag(GetWorld(), FName("MainCamera"), FoundActors);
+
+	if (FoundActors.Num() == 0)
+	{
+		UE_LOG(LogTemp, Error, TEXT("No actors with tag 'MainCamera' found!"));
+		return;
+	}
+
+	// Utiliser le premier acteur trouvé
+	ACineCameraActor* MainCamera = Cast<ACineCameraActor>(FoundActors[0]);
+	if (!MainCamera)
+	{
+		UE_LOG(LogTemp, Error, TEXT("Found actor is not a CineCameraActor!"));
+		return;
+	}
+
+	// Définir la caméra comme vue principale
+	PlayerController->SetViewTarget(MainCamera);
+}
---
+++ Source/KnightKombo/MainGameMode.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MainGameMode.generated.h"
+
+UCLASS()
+class KNIGHTKOMBO_API AMainGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	virtual void BeginPlay() override;
+
+private:
+	void SetupPlayerCamera();
+};
